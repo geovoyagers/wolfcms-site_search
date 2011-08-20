8c03b140739d1fa1edf1caaaf82a7d9d9d09c8b6
Tina Keil <seven@geovoyagers.de>
2011-08-20 23:13:00 +0200

    Version 1.0.0

---
+++ uninstall.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * Wolf CMS - Content Management Simplified. <http://www.wolfcms.org>
+ * Copyright (C) 2008-2010 Martijn van der Kleijn <martijn.niji@gmail.com>
+ *
+ * This file is part of Wolf CMS. Wolf CMS is licensed under the GNU GPLv3 license.
+ * Please see license.txt for the full license text.
+ */
+
+/* Security measure */
+if (!defined('IN_CMS')) { exit(); }
+
+/**
+ * This plugin allows you to integrate a search function on your site.
+ *
+ * @package plugins
+ * @subpackage site_search
+ *
+ * @author Tina Keil <seven@geovoyagers.de>
+ * @version 1.0.0
+ * @since Wolf version 0.7.5
+ * @license http://www.gnu.org/licenses/gpl.html GPL License
+ * @copyright Tina Keil, 2011+
+ */
+
+exit();
